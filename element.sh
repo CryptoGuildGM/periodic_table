911bcdc4bc843b53dadeaf138199c078db6ca38c
CryptoGuildGM <cryptoguildgrandmaster313@gmail.com>
2022-07-16 23:29:31 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#A simple Bash/SQL project from freecodecamp to retrieve data about atomic elements from a postgres database.
+
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[  -z $1  ]]
+then
+  echo "Please provide an element as an argument."
+fi
+
+#if atomic number 
+re='^[0-9]+$'
+if [[  $1 =~ $re ]]
+then
+  FIND_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+
+  #if no element found
+  if [[  -z $FIND_ELEMENT_RESULT  ]]
+  then
+    echo "I could not find that element in the database."
+    exit
+  fi
+
+  #output the result
+  echo $FIND_ELEMENT_RESULT | while IFS=" |" read TYPE_ID NUMBER SYMBOL NAME MASS MELTING_POINT BOILING_POINT TYPE
+    do
+      echo "The element with atomic number $1 is $NAME. It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done 
+fi
+
+#if atomic symbol
+re2='^[ABC]+$'
+if [[  $1 =~ $re2 ]]
+then
+  FIND_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1'")
+  
+  #if no element found
+  if [[  -z $FIND_ELEMENT_RESULT  ]]
+  then
+    echo "I could not find that element in the database."
+    exit
+  fi
+
+  #output the result
+  echo $FIND_ELEMENT_RESULT | while IFS=" |" read TYPE_ID NUMBER SYMBOL NAME MASS MELTING_POINT BOILING_POINT TYPE
+    do
+      echo "The element with atomic number $1 is $NAME. It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done 
+fi
+
+#if name
+#if no element found
